93bc3ed6cefedca0cbd1079ad85a10849fe93ee0
imran1004m <imranmunshi619@gmail.com>
2025-05-27 07:50:42 +0530

    project files and folders created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Imran M",
+    author_email="imranmunshi619@gmail.com",
+    packages=find_packages()
+)
